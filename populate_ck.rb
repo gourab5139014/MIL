822189e5516eb3191bb312ea4d4749fc25c8e3a7
Gourab Mitra <gourab5139014@gmail.com>
2012-03-23 01:21:01 +0530

    files to provide c,k to program in CSV

---
+++ populate_ck.rb
@@ -0,0 +1,11 @@
+#This file populates the ck.data file which provides values of C,K parameters in CSV
+#Modify the following definitions of c and K. THEY SHOULD BE OF EQUAL LENGTH
+c= 1..10
+k= 1..10
+
+File.open('ck.data','w') do |f|
+	c.zip(k).each do |first,second|
+#		f.printf("%d,%d\n",first,second)
+		f.puts "#{first},#{second}\n"
+	end
+end
