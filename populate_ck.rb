0a37443943c1fc38d7b2e759e53a1b16badd3de1
Gourab Mitra <gourab5139014@gmail.com>
2012-03-28 22:30:40 +0530

    added ckIRIS.data to tracking

--- populate_ck.rb
+++ populate_ck.rb
@@ -1,11 +1,10 @@
 #This file populates the ck.data file which provides values of C,K parameters in CSV
 #Modify the following definitions of c and K. THEY SHOULD BE OF EQUAL LENGTH
-c= 1..10
-k= 1..10
+c= 1..5
+k= [2,3,7,8,9]
 
 File.open('ckIRIS.data','w') do |f|
 	c.zip(k).each do |first,second|
-#		f.printf("%d,%d\n",first,second)
 		f.puts "#{first},#{second}\n"
 	end
 end
